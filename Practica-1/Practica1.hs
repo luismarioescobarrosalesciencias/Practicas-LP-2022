b7397fadea75691715040178ecc5c6fba71dab1c
IsraelNightray <israel_nightray@ciencias.unam.mx>
2022-04-01 23:01:14 -0600

    Se actualizo eval1 con los requerimientos solicitados

--- Practica-1/Practica1.hs
+++ Practica-1/Practica1.hs
@@ -34,52 +34,98 @@ fv (Let a1 a2) = fv a1 ++ fv a2
 fv (Abs x a1) = filter (/= x) (fv a1)
 
 subs :: EAB -> Subst -> EAB
-subs (Var s) (x,e) = if s == x
+subs (Var s) (x, e) = if s == x
                      then e
                      else Var s
 subs (Num n) _ = Num n
-subs (Add a1 a2) s = Add (subs a1 s) (subs a2 s)
+subs (And a1 a2) s = And (subs a1 s) (subs a2 s)
+subs (Or a1 a2) s = Or (subs a1 s) (subs a2 s)
+subs (Not a) s = Not (subs a s)
+subs (Pred a) s = Pred (subs a s)
+subs (Neg a) s = Neg (subs a s)
+subs (Suc a) s = Suc (subs a s)
+subs (Sum a1 a2) s = Sum (subs a1 s) (subs a2 s)
 subs (Prod a1 a2) s = Prod (subs a1 s) (subs a2 s)
 subs (Let a1 a2) s = Let (subs a1 s) (subs a2 s)
+subs (Iszero a) s = Iszero (subs a s)
+subs (If a1 a2 a3) s = If (subs a1 s) (subs a2 s) (subs a3 s)
 subs (Abs z e) s@(x,r)
  | z==x || elem z (fv r) = error "error random"
  | otherwise = Abs z (subs e s)
 
 eval1 :: EAB -> EAB
-eval1 (Var x) = error "Variable libre"
-eval1 (Num n) = n
-eval1 (Bool b)= b
-eval1 (Sum a b) = eval1 a + eval1 b
-eval1 (Prod a b)= eval1 a * eval1 b
-eval (Neg e) = eval1 e * -1
-eval1 (Pred n) = eval1 n -1
-eval1 (Suc n) = eval1 n +1
-eval1 (And a b) = eval1 a && eval1 b
-eval1 (Or a b) = eval1 a || eval1 b
-eval1 (Iszero n) = if eval1 n ==0
-                   then true 
-                   else false 
+eval1 (Var x) = Var x
+eval1 (Num n) = Num n
+eval1 (Bool b)= Bool b
+eval1 (Sum a b) = case (a, b) of
+                (Num c, Num d) -> Num (c + d)
+                (Num c, e) -> Sum (Num c) (eval1 e)
+                (c, Num e) -> Sum (eval1 c) (Num e)
+                (c, d) -> Sum (eval1 c) (eval1 d)
+eval1 (Prod a b)= case (a, b) of
+                (Num c, Num d) -> Num (c * d)
+                (Num c, e) -> Prod (Num c) (eval1 e)
+                (c, Num e) -> Prod (eval1 c) (Num e)
+                (c, d) -> Prod (eval1 c) (eval1 d)
+eval1 (Neg e) = case e of
+                (Num c) -> (Num (-c))
+                (c) -> Prod (c) (Neg (Num 1))
+eval1 (Pred n) = case n of
+                (Num c) -> Num (c - 1) 
+                (c) -> Sum (c) (Neg (Num 1))
+eval1 (Suc n) = case n of
+                (Num c) -> Num (c + 1) 
+                (c) -> Sum (c) (Num 1)
+eval1 (Not a) = case a of
+                (Bool True) -> Bool False
+                (Bool False) -> Bool True
+                (c) -> Not (eval1 a)  
+eval1 (And a b) = case (a, b) of
+                (Bool True, Bool True) -> Bool True
+                (Bool False, Bool False) -> Bool False
+                (Bool False, c) -> Bool False
+                (c, Bool False) -> Bool False
+                (Bool True, c) -> And (Bool True) (eval1 c)
+                (c, Bool True) -> And (eval1 c) (Bool True)
+                (c, d) -> And (eval1 c) (eval1 d)
+eval1 (Or a b) = case (a, b) of
+                (Bool True, Bool True) -> Bool True
+                (Bool False, Bool False) -> Bool False
+                (Bool True, c) -> Bool True
+                (c, Bool True) -> Bool True
+                (Bool False, c) -> Or (Bool False) (eval1 c)
+                (c, Bool False) -> Or (eval1 c) (Bool False)
+                (c, d) -> Or (eval1 c) (eval1 d)
+eval1 (Iszero n) = case n of 
+                  (Num c) -> if c == 0
+                             then Bool True
+                             else Bool False  
+                  (c) -> Iszero (eval1(c))            
 eval1 (If a b c) = case a of
-                    (i) -> If(eval1(i) b c)
-                    (True)  -> b    
-                    (False) -> c    
-eval1 (Let e1 (Abs x e2))= eval1 (subs e2 (x, e1))
-eval1 (Abs s e) = eval1 (subs e s) -- ¿Estaria bien?
+                    (Bool True)  -> b    
+                    (Bool False) -> c  
+                    (d) -> If (eval1 d)(b)(c)
+eval1 (Let e1 (Abs x e2))= case e1 of
+                    (Num a) -> subs e2 (x, Num a)
+                    (Bool b) -> subs e2 (x, Bool b)
+                    (Var s) -> subs e2 (x, Var s)
+                    (c) -> Let (eval1 c) (Abs (x) (e2))
+eval1 (Abs s e) = Abs (s) (eval1 e) 
+                  
 
-evals :: EAB -> EAB
-evals (Var x) = error "Variable libre"
-evals (Num n) = n
-evals (Bool b)= b
+--evals :: EAB -> EAB
 
-eval :: EAB -> EAB
-eval _ = error "Implementar"
+
+
+--eval :: EAB -> EAB
+--eval _ = error "Implementar"
 
 data Type = TypeN -- Para Numeros
            |TypeB  -- Para Booleanos
 type Ctx = [(String,Type)] -- [("y", TypeN)] para contextos.
 
-vt :: Ctx -> EAB -> Type -> Bool
-vt _ _ _ = error "Implementar"
+--vt :: Ctx -> EAB -> Type -> Bool
+--vt _ _ _ = error "Implementar"
 
-evalt :: EAB -> EAB
-evalt _ = error "Implementar"
+--evalt :: EAB -> EAB
+--evalt _ = error "Implementar"
