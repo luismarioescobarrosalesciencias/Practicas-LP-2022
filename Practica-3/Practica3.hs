a9085a3392b586bb057530ffacbb69edd5678f1e
MaiteDT <digit@ciencias.unam.mx>
2022-05-30 20:58:30 -0500

    iniciando eval1

--- Practica-3/Practica3.hs
+++ Practica-3/Practica3.hs
@@ -91,3 +91,35 @@ subst (Assign e1 e2) s = Assign (subst e1 s) (subst e2 s)
 subst (Void) s = Void
 subst (Seq e1 e2) s = Seq (subst e1 s) (subst e2 s)
 subst (While e1 e2) s = While (subst e1 s) (subst e2 s)
+
+eval1 :: (Memory , Expr ) -> (Memory , Expr)
+eval1 (mem, Var x) = (mem, Var x)
+eval1 (mem, B x) = (mem, B x)
+eval1 (mem, I x) = (mem, I x)
+eval1 (mem, (Add e1 e2)) = case (e1, e2) of 
+                           ((I e1), (I e2)) -> (mem, I (e1+e2))
+                           (s , c) -> (mem, Add e1 e2) --O marcar error???
+eval1 (mem, (Mul e1 e2)) = case (e1, e2) of 
+                           ((I e1), (I e2)) -> (mem, I (e1*e2))
+                           (s , c) -> (mem, Mul e1 e2)
+eval1 (mem, Succ e) = case e of
+                      (I x) -> (mem, I (x+1))
+                      (_) -> error "No se ingreso un numero"
+eval1 (mem, Pred e) = case e of
+                      (I x) -> (mem, I (x-1))
+                      (_) -> error "No se ingreso un numero"
+eval1 (mem, (And e1 e2)) = case (e1, e2) of 
+                           ((B e1), (B e2)) -> (mem, B (e1 && e2))
+                           (s , c) -> (mem, And e1 e2)
+eval1 (mem, (Or e1 e2)) = case (e1, e2) of 
+                           ((B e1), (B e2)) -> (mem, B (e1 || e2))
+                           (s , c) -> (mem, Or e1 e2)
+eval1 (mem, Not e) = case e of 
+                    (B e) -> (mem, B (not e))
+                    (_) -> error "Lo que se ingresó no es booleano"
+eval1 (mem, Iszero e) = case e of 
+                        (I e) -> if e==0
+                                then (mem, B True)
+                                else (mem, B False)
+                        (_) -> error "No se puede procesar"
+                    
